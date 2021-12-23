1bfff224312c7b849fcf5c4d2144fa6654c4e215
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-12-23 18:19:35 -0300

    add Next Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-
+import Link from "next/link";
 
 function Home() {
-    return <h1>New Home test</h1>
+    return (
+        <>
+            <h1>Home</h1>
+            <Link href="/counter">
+                <a>Contador</a>
+            </Link>
+        </>
+    )
 }
 
 export default Home;
