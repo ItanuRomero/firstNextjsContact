94a46fe265d880b9f82a0c5c29cbddfd59617b94
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-12-23 18:11:33 -0300

    create counter page and remove impolementation from home

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
-import { useState } from "react";
+
 
 function Home() {
-    return (
-        <>
-        <h1>Home</h1>
-        <Contador />
-        </>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(0);
-    function addContador() {
-        setContador(contador + 1);
-    }
-    return (
-        <>
-            <h1>{contador}</h1>
-            <button onClick={addContador}>Contador++</button>
-        </>
-    )
+    return <h1>Home</h1>
 }
 
 export default Home;
